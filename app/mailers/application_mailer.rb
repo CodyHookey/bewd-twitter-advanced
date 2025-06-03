84f475a1c382bccc516e5f17e3cbb2fb651dbb6e
Cody Hookey <codyhookey@outlook.com>
2025-06-03 15:22:34 +0100

    added mailer and tested
    - had to use mailersend instead of mailgun

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'cody@test-eqvygm0r5yjl0p7w.mlsender.net'
   layout 'mailer'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => ENV['MAILERSEND_SMTP_SERVER'],
+  :port => ENV['MAILERSEND_SMTP_PORT'],
+  :authentication => :plain,
+  :user_name => ENV['MAILERSEND_SMTP_LOGIN'],
+  :password => ENV['MAILERSEND_SMTP_PASSWORD'],
+  :domain => ENV['MAILERSEND_DOMAIN'],
+  :enable_starttls_auto => true
+}
+
+ActionMailer::Base.default_url_options = { host: ENV['URL'] }
